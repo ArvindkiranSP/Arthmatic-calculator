0aed7bd1f7efce36bbb7f8c8f2430180b39e3b9f
LENOVO <LENOVO@DESKTOP-8KSFVBQ>
2023-11-02 22:04:44 +0530

    Add project files.

---
+++ Arthmatic calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arthmatic_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            var operater= Convert.ToString(Console.ReadLine());
+            var input1= int.Parse(Console.ReadLine());
+
+            arthmaticcalculator arthm = new arthmaticcalculator();
+            var result = arthm.GetResult(input, input1, operater);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Arthmatic calculator/arthmaticcalculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arthmatic_calculator
+{
+    internal class arthmaticcalculator
+    {
+        public int GetResult(int x, int y, string operaters) 
+        { 
+            var result = 0;
+            switch (operaters) 
+            {
+                case "+":
+                    {
+                        result = x + y;
+                       return result;
+                    }
+                    case "-":
+                    {
+                        result = x - y;
+                        return result;
+                    }
+                    case "*":
+                    {
+                        result = x * y;
+                        return result;
+                    }
+                    case "/":
+                    
+                    {
+                        result = x / y;
+                        return result;
+                    }
+                default:
+                    {
+                        return  0;
+                    }
+
+            }
+        }
+    }
+}
